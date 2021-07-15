6e744b5a846b86be7f2aeee7b947307ecd8437ce
Huahai Yang <huahai.yang@gmail.com>
2021-07-14 18:52:29 -0700

    update to 0.4.40

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -1,15 +1,15 @@
 class Datalevin < Formula
   desc "Simple, fast and durable Datalog database"
   homepage "https://github.com/juji-io/datalevin"
-  version "0.4.39"
+  version "0.4.40"
   license "EPL-1.0"
 
   if OS.linux?
-    url "https://github.com/juji-io/datalevin/releases/download/0.4.39/dtlv-0.4.39-ubuntu-latest-amd64.zip"
-    sha256 "3bf378e4afd917ea1d13cb7d4ab214e2d7d09417281c7553f21fd3cfc7648181"
+    url "https://github.com/juji-io/datalevin/releases/download/0.4.40/dtlv-0.4.40-ubuntu-latest-amd64.zip"
+    sha256 "e79383b12fb747d959775f956384087e4c14b0226852db567309ad07ceb4a977"
   else
-    url "https://github.com/juji-io/datalevin/releases/download/0.4.39/dtlv-0.4.39-macos-latest-amd64.zip"
-    sha256 "8dc64d6d63a4ba1a9a4dff6492213dac4f4b9f682c1db7094aa107ceb0a26936"
+    url "https://github.com/juji-io/datalevin/releases/download/0.4.40/dtlv-0.4.40-macos-latest-amd64.zip"
+    sha256 "bbe319f50dd424a20d8ede3166a9779f9c06753aaee0689426dd8386dbadd748"
   end
 
   bottle :unneeded
