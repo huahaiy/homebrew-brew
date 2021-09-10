e9c82840f00acc2a277abcac21f351c0db9ed6da
Huahai Yang <huahai.yang@gmail.com>
2021-09-10 16:24:14 -0700

    Revert "update to on_os as suggested"
    This reverts commit 5fafe1797e9aeecfc2ac7d7cef658c1eb44a77a5.

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -4,12 +4,10 @@ class Datalevin < Formula
   version "0.5.8"
   license "EPL-1.0"
 
-  on_linux do
+  if OS.linux?
     url "https://github.com/juji-io/datalevin/releases/download/0.5.8/dtlv-0.5.8-ubuntu-latest-amd64.zip"
     sha256 "aa966a9906acdc0e80768b5744f093978f44176fb3c4d55ff9e0b9cea58ef638"
-  end
-
-  on_macos do
+  else
     url "https://github.com/juji-io/datalevin/releases/download/0.5.8/dtlv-0.5.8-macos-latest-amd64.zip"
     sha256 "d051099a857083ec7a3fbfa11bf8262c0537d6d96acd12bcdadfecc21ef9b369"
   end
