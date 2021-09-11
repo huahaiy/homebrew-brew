26a8f9991ae915d95343410cf07f9114f93cd77a
Huahai Yang <huahai.yang@gmail.com>
2021-09-10 21:49:06 -0700

    update

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -6,14 +6,14 @@ class Datalevin < Formula
 
   bottle :unneeded
 
-  on_linux do
-    url "https://github.com/juji-io/datalevin/releases/download/0.5.8/dtlv-0.5.8-ubuntu-latest-amd64.zip"
-    sha256 "aa966a9906acdc0e80768b5744f093978f44176fb3c4d55ff9e0b9cea58ef638"
-  end
-
   on_macos do
     url "https://github.com/juji-io/datalevin/releases/download/0.5.8/dtlv-0.5.8-macos-latest-amd64.zip"
     sha256 "d051099a857083ec7a3fbfa11bf8262c0537d6d96acd12bcdadfecc21ef9b369"
+  end
+
+  on_linux do
+    url "https://github.com/juji-io/datalevin/releases/download/0.5.8/dtlv-0.5.8-ubuntu-latest-amd64.zip"
+    sha256 "aa966a9906acdc0e80768b5744f093978f44176fb3c4d55ff9e0b9cea58ef638"
   end
 
   def install
