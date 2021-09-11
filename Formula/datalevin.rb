9a0dd85b91134664104d573652ee54b853fe75e7
Huahai Yang <huahai.yang@gmail.com>
2021-09-10 21:45:31 -0700

    update

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -11,7 +11,7 @@ class Datalevin < Formula
     sha256 "aa966a9906acdc0e80768b5744f093978f44176fb3c4d55ff9e0b9cea58ef638"
   end
 
-  on_linux do
+  on_macos do
     url "https://github.com/juji-io/datalevin/releases/download/0.5.8/dtlv-0.5.8-macos-latest-amd64.zip"
     sha256 "d051099a857083ec7a3fbfa11bf8262c0537d6d96acd12bcdadfecc21ef9b369"
   end
