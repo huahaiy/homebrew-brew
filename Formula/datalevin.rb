00f5b1d9c96cda0823548660341b540606baaab2
Huahai Yang <huahai.yang@gmail.com>
2021-09-16 14:35:46 -0700

    fix formula

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -7,14 +7,19 @@ class Datalevin < Formula
   bottle :unneeded
 
   if OS.linux?
-    url "https://github.com/juji-io/datalevin/releases/download/0.5.14/dtlv-0.5.14-macos-latest-amd64.zip"
+    url "https://github.com/juji-io/datalevin/releases/download/0.5.14/dtlv-0.5.14-ubuntu-latest-amd64.zip"
     sha256 "30416d0b6f261e483a5b081f9d5ac35b2fbf2e3bbd9931f133fa4cb314571e35"
   else
-    url "https://github.com/juji-io/datalevin/releases/download/0.5.14/dtlv-0.5.14-ubuntu-latest-amd64.zip"
+    url "https://github.com/juji-io/datalevin/releases/download/0.5.14/dtlv-0.5.14-macos-latest-amd64.zip"
     sha256 "c93a1b75ac2f45f2754505521381400d220ec12a626713cb0b893f379691e22a"
   end
 
   def install
     bin.install "dtlv"
+  end
+
+  test do
+    assert_equal "10\n",
+      pipe_output("#{bin}/dtlv exec '(+ 1 2 3 4)'")
   end
 end
