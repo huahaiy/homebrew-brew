a7c58e880f3f7457cd70e361adb8cf60f6efbafd
Huahai Yang <huahai.yang@gmail.com>
2021-11-19 10:16:45 -0800

    update

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -4,11 +4,9 @@ class Datalevin < Formula
   version "0.5.27"
   license "EPL-1.0"
 
-  bottle :unneeded
-
   if OS.linux?
     url "https://github.com/juji-io/datalevin/releases/download/0.5.27/dtlv-0.5.27-ubuntu-latest-amd64.zip"
-    sha256 "3725deb862b3e4490cba833367f2ea42d3de2b578867c02f6d9c102fdd6671f1"
+    sha256 "3ad2cc5f8b4c4b391a31a689c10b597fc729f523c0872e971e83dfca1de3406e"
   else
     url "https://github.com/juji-io/datalevin/releases/download/0.5.27/dtlv-0.5.27-macos-latest-amd64.zip"
     sha256 "7ba195f469ac69b4774263d15cbd7d385421012d1143371ac65ecf3871993263"
