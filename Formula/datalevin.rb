87259ba92d6d9d7eb04983aa6aea2cd799d38663
Huahai Yang <huahai.yang@gmail.com>
2022-05-24 09:43:50 -0700

    update

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -1,15 +1,15 @@
 class Datalevin < Formula
   desc "Simple, fast and versatile Datalog database"
   homepage "https://github.com/juji-io/datalevin"
-  version "0.6.12"
+  version "0.6.13"
   license "EPL-1.0"
 
   if OS.linux?
-    url "https://github.com/juji-io/datalevin/releases/download/0.6.12/dtlv-0.6.12-ubuntu-latest-amd64.zip"
-    sha256 "f2bc090eaa9d338a4044ac43db0708075b9af78c18b63c86798d9d5f61bee806"
+    url "https://github.com/juji-io/datalevin/releases/download/0.6.13/dtlv-0.6.13-ubuntu-latest-amd64.zip"
+    sha256 "2daf2276ddd0c39f3ddc4edd08d71f6f2f08fd033fd64ce4faf06a8f568249a9"
   else
-    url "https://github.com/juji-io/datalevin/releases/download/0.6.12/dtlv-0.6.12-macos-latest-amd64.zip"
-    sha256 "3542354ec14561eea72a3ead4de528405694386565ffe56edd2150ddb9ba25e2"
+    url "https://github.com/juji-io/datalevin/releases/download/0.6.13/dtlv-0.6.13-macos-latest-amd64.zip"
+    sha256 "fbbf97fbb2d0b69b9af4e3ffa4d15315e247533327b7fc046ef6944c23a06236"
   end
 
   def install
