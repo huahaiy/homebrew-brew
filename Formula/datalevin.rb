877e0396228c335a9017b824e5e5547ca6ff20e4
Huahai Yang <huahai.yang@gmail.com>
2026-01-24 16:49:39 -0800

    update

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -1,20 +1,15 @@
 class Datalevin < Formula
   desc "Simple, fast and versatile Datalog database"
-  homepage "https://github.com/juji-io/datalevin"
-  version "0.9.27"
+  homepage "https://github.com/datalevin/datalevin"
+  version "0.10.2"
   license "EPL-1.0"
 
   if OS.linux?
-    url "https://github.com/juji-io/datalevin/releases/download/0.9.27/dtlv-0.9.27-ubuntu-22.04-amd64.zip"
-    sha256 "48b1e75a52441c5d11cdbad9b98c0daf6763c011d960cc98881dae7ae091dbea"
+    url "https://github.com/datalevin/datalevin/releases/download/0.10.2/dtlv-0.10.2-ubuntu-22.04-amd64.zip"
+    sha256 "eb23d179ba284651fb5a6a7c289788c66ab094f4829aa7264c8d1822282b1dd4"
   else
-    if Hardware::CPU.arm?
-      url "https://github.com/juji-io/datalevin/releases/download/0.9.27/dtlv-0.9.27-macos-latest-aarch64.zip"
-      sha256 "e5fe9e66f7fb67993a20b885079d1a1f8aebf5be634eb2a2e45f3275a9af6081"
-    else
-      url "https://github.com/juji-io/datalevin/releases/download/0.9.27/dtlv-0.9.27-macos-13-amd64.zip"
-      sha256 "2a4f89758d39c87e60abe2ea297478cfb9417cf6d84992177d7e48ba28f84627"
-    end
+    url "https://github.com/datalevin/datalevin/releases/download/0.10.2/dtlv-0.10.2-macos-14-aarch64.zip"
+    sha256 "0f4ba20f5e8d7d2889f78cec350745fbfe7b2df08e2b0a52ffcdc3901ab370d4"
   end
 
   def install
