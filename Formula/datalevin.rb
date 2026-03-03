015704ddd986bd2681eaacd2ebaf1d1d4d5a38b6
Huahai Yang <huahai.yang@gmail.com>
2026-03-03 13:52:10 -0800

    update

--- Formula/datalevin.rb
+++ Formula/datalevin.rb
@@ -1,15 +1,15 @@
 class Datalevin < Formula
   desc "Simple, fast and versatile Datalog database"
   homepage "https://github.com/datalevin/datalevin"
-  version "0.10.5"
+  version "0.10.7"
   license "EPL-1.0"
 
   if OS.linux?
-    url "https://github.com/datalevin/datalevin/releases/download/0.10.5/dtlv-0.10.5-ubuntu-22.04-amd64.zip"
-    sha256 "fb002bd3775124aa890941be29e61e87f05cc92ecc70e36e524dbe366a9bf1e4"
+    url "https://github.com/datalevin/datalevin/releases/download/0.10.7/dtlv-0.10.7-ubuntu-22.04-amd64.zip"
+    sha256 "ed7c1d3a1cc7cf4c6ab89b5e1b227436988dabbf98f0c9bec8ae3cbb4919b085"
   else
-    url "https://github.com/datalevin/datalevin/releases/download/0.10.5/dtlv-0.10.5-macos-14-aarch64.zip"
-    sha256 "540d1198378ed10b61a724cd184ef700d01aa909f93212966357e6f8f1197460"
+    url "https://github.com/datalevin/datalevin/releases/download/0.10.7/dtlv-0.10.7-macos-14-aarch64.zip"
+    sha256 "c6089e2bd8842ef89fa1808a8f9064c4de943fff08e505e88a7ce6b10e132a0a"
   end
 
   def install
